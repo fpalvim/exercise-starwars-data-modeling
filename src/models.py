2530098a7de809091cb5193f0acd4b693664ad64
Felipe Alvim <128483571+fpalvim@users.noreply.github.com>
2024-07-17 19:33:48 +0000

    starwars data modeling proj

--- src/models.py
+++ src/models.py
@@ -7,12 +7,14 @@ from eralchemy2 import render_er
 
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
-    # Here we define columns for the table person
+class User(Base):
+    __tablename__ = 'user'
+    # Here we define columns for the table user
     # Notice that each column is also a normal Python instance attribute.
     id = Column(Integer, primary_key=True)
     name = Column(String(250), nullable=False)
+    charachter_id = Column(Integer, ForeignKey('charachter.id'))
+    planet_id = Column(Integer, ForeignKey('planet.id'))
 
 class Address(Base):
     __tablename__ = 'address'
@@ -22,8 +24,22 @@ class Address(Base):
     street_name = Column(String(250))
     street_number = Column(String(250))
     post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
+    user_id = Column(Integer, ForeignKey('user.id'))
+    user = relationship(User)
+
+class Charachter(Base):
+    __tablename__ = 'charachter'
+    # Here we define columns for the table charachter
+    # Notice that each column is also a normal Python instance attribute.
+    id = Column(Integer, primary_key=True)
+    name = Column(String(250), nullable=False)
+
+class Planet(Base):
+    __tablename__ = 'planet'
+    # Here we define columns for the table planet
+    # Notice that each column is also a normal Python instance attribute.
+    id = Column(Integer, primary_key=True)
+    name = Column(String(250), nullable=False)
 
     def to_dict(self):
         return {}

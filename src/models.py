0cc8ba4de6ab95fa6b4884d6691e8734c2d4652d
Felipe Alvim <128483571+fpalvim@users.noreply.github.com>
2024-07-19 18:58:38 +0000

    corrections on the foreign keys

--- src/models.py
+++ src/models.py
@@ -7,26 +7,6 @@ from eralchemy2 import render_er
 
 Base = declarative_base()
 
-class User(Base):
-    __tablename__ = 'user'
-    # Here we define columns for the table user
-    # Notice that each column is also a normal Python instance attribute.
-    id = Column(Integer, primary_key=True)
-    name = Column(String(250), nullable=False)
-    charachter_id = Column(Integer, ForeignKey('charachter.id'))
-    planet_id = Column(Integer, ForeignKey('planet.id'))
-
-class Address(Base):
-    __tablename__ = 'address'
-    # Here we define columns for the table address.
-    # Notice that each column is also a normal Python instance attribute.
-    id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    user_id = Column(Integer, ForeignKey('user.id'))
-    user = relationship(User)
-
 class Charachter(Base):
     __tablename__ = 'charachter'
     # Here we define columns for the table charachter
@@ -40,6 +20,30 @@ class Planet(Base):
     # Notice that each column is also a normal Python instance attribute.
     id = Column(Integer, primary_key=True)
     name = Column(String(250), nullable=False)
+
+class User(Base):
+    __tablename__ = 'user'
+    # Here we define columns for the table user
+    # Notice that each column is also a normal Python instance attribute.
+    id = Column(Integer, primary_key=True)
+    name = Column(String(250), nullable=False)
+    charachter_id = Column(Integer, ForeignKey('charachter.id'))
+    charachter = relationship(Charachter)
+    planet_id = Column(Integer, ForeignKey('planet.id'))
+    planet = relationship(Planet)
+
+# class Address(Base):
+#     __tablename__ = 'address'
+#     # Here we define columns for the table address.
+#     # Notice that each column is also a normal Python instance attribute.
+#     id = Column(Integer, primary_key=True)
+#     street_name = Column(String(250))
+#     street_number = Column(String(250))
+#     post_code = Column(String(250), nullable=False)
+#     user_id = Column(Integer, ForeignKey('user.id'))
+#     user = relationship(User)
+
+
 
     def to_dict(self):
         return {}
